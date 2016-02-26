7f02cb355bf7e0127a6e942d37c8b96cfdb5309f
liuitt <liuitt@liuitt.com>
2016-02-26 11:26:35 -0300

    Update AbstractContainer.php

--- src/Contract/Action/AbstractContainer.php
+++ src/Contract/Action/AbstractContainer.php
@@ -1,102 +1,60 @@
 <?php namespace Liuitt\Component\Container\Contract\Action;
 
-use Liuitt\Component\Container\Contract\Type\ContainerInterface;
-
 use ReflectionClass;
 
-trait Intantiable
-{
-    protected static $instance;
+use Liuitt\Component\Container\Contract\Type\ContainerInterface;
 
-    final public static function getInstance()
+abstract class AbstractContainer implements ContainerInterface
+{
+
+    protected static $instance = null;
+    private static $containers = [];
+    private static $args = [];
+
+    public function arg($key, $value = null)
+    {
+        self::$args[$key] = $value;
+    }
+
+    public static function register(String $alias, Callable $callable )
+    {
+        self::getInstance();
+        self::$containers[$alias] = $callable;
+    }
+
+    public static function registerArray(Array $data)
+    {
+
+    }
+
+    private static function resolveInternal($alias, $args)
+    {
+        return call_user_func_array(self::$containers[$alias], $args);
+    }
+
+    public static function resolve(String $alias)
+    {
+        return self::resolveInternal($alias, [self::getInstance()]);
+    }
+
+    public static function resolveWith(String $aliasOrNamespace, $args)
+    {
+        return self::resolveInternal($aliasOrNamespace, $args);
+    }
+
+    public static function dispose(String $aliasOrNamespace)
+    {
+
+    }
+
+    final private static function getInstance()
     {
         if (!isset(static::$instance)) {
-            $class = new ReflectionClass(__CLASS__);
+            $class = new ReflectionClass('\Liuitt\Component\Container');
             static::$instance = $class->newInstanceArgs(func_get_args());
         }
 
         return static::$instance;
     }
 
-    final private function __clone()
-    {
-    }
-
-    final private function __wakeup()
-    {
-    }
 }
- 
-abstract class AbastractContainer implements ContainerInterface
-{
-	use Intantiable;
-	
-	private static $data = [];
-	
-	public function __call($name, $args)
-	{
-		return self::call($name, $args);
-	}
-	
-	public function __callStatic($name, $args)
-	{
-		return self::call($name, $args);
-	}
-	
-	public function call($name, $args)
-	{
-		// register
-		
-		// resolve
-	}
-	
-	public function register(String $className, String $alias)
-	{
-		
-	}
-	
-	public function registerArray(Array $data)
-	{
-		
-	}
-	
-	public function resolve(String $aliasOrNamespace)
-	{
-		
-	}
-
-	public function resolveWith(String $aliasOrNamespace, Array $params)
-	{
-		
-	}
-	
-	public function dispose(String $aliasOrNamespace)
-	{
-		
-	}
-	
-}
-
-$container = new Container();
-$container->setEnvironment('dev', 'prod', 'homolog');
-
-$product = $container->register(new Product($id), 'Product');
-$product = $container->registerProduct($id);
-
-$container->registerArray([
-	'Produto' => [ new Product($id), 'Product' ],
-	'Item' => new CartItem($container->resolveProduct())
-]);
-
-$item = $container->register(new CartItem($product));
-$item = $container->register(new CartItem($container->resolve('Product')));
-$item = $container->register(new CartItem($container->resolveProduct()));
-$item = $container->registerWith('CartItem', [$container->resolveProduct()]);
-$item = $container->registerWith('CartItem', [$container->resolveWith('Product', [1])])
-
-Container::resolveProductFromHomolog();
-Container::resolveProductFromHomologWith([1]);
-
-
-
-
